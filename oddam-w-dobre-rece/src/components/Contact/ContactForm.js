1a986065605c36293e76aeda0da4f35fe73999be
Kaja Piechówka <kaja.piechowka@gmail.com>
2020-11-24 17:22:44 +0100

    form validation done

--- oddam-w-dobre-rece/src/components/Contact/ContactForm.js
+++ oddam-w-dobre-rece/src/components/Contact/ContactForm.js
@@ -1,9 +1,22 @@
 import React from "react";
 import DecorationImg from "../repetable/DecorationImg";
+import { useForm } from "react-hook-form";
+
+const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; //eslint-disable-line
 
 const ContactForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    fetch("https://fer-api.coderslab.pl/v1/portfolio/contact", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    });
+  };
+
   return (
-    <form className="contact__form">
+    <form onSubmit={handleSubmit(onSubmit)} className="contact__form">
       <h4 className="contact__title">Skontaktuj się z nami</h4>
       <DecorationImg />
       <div className="contact__names">
@@ -17,7 +30,10 @@ const ContactForm = () => {
             id="name"
             name="name"
             placeholder="Krzysztof"
+            ref={register({ required: true, minLength: 2 })}
+            style={errors ? { borderBottom: "1px solid red" } : null}
           />
+          {errors.name && <p className="error">Imię jest za krótkie!</p>}
         </div>
         <div>
           <label className="contact__label" htmlFor="email">
@@ -29,7 +45,13 @@ const ContactForm = () => {
             id="email"
             name="email"
             placeholder="abc@xyz.pl"
+            ref={register({
+              required: true,
+              pattern: re,
+            })}
+            style={errors ? { borderBottom: "1px solid red" } : null}
           />
+          {errors.email && <p className="error">Email jest nieprawidłowy!</p>}
         </div>
       </div>
       <div className="contact__message">
@@ -37,9 +59,15 @@ const ContactForm = () => {
           Wpisz swoją wiadomość
         </label>
         <textarea
+          name="message"
           className="contact__textaeria"
+          ref={register({ required: true, min: 120 })}
           placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          style={errors ? { borderBottom: "1px solid red" } : null}
         />
+        {errors.message && (
+          <p className="error">Wiadomość musi mieć conajmniej 120 znaków</p>
+        )}
       </div>
       <div className="contact__send">
         <button type="submit" className="contact__button">
